89b5bd79b5f949d96b0a2fe3b6d240f2da4d14bc
Hillal-cell <sserunjogi.hillal@students.mak.ac.ug>
2024-05-05 23:22:12 +0300

    Data base handler to use the rigth parameters.

--- Manager/app/src/test/java/com/hillal/taskmanager/database/OnDataBaseAction_ImplTest.java
+++ Manager/app/src/test/java/com/hillal/taskmanager/database/OnDataBaseAction_ImplTest.java
@@ -1,4 +1,45 @@
+package com.hillal.taskmanager.database;
+
 import junit.framework.TestCase;
+
 public class OnDataBaseAction_ImplTest extends TestCase {
-  
+
+    public void testInsertDataIntoTaskList() {
+    }
+
+    public void testTruncateTheList() {
+    }
+
+    public void testTruncateTheSubList() {
+    }
+
+    public void testDeleteTaskFromId() {
+    }
+
+    public void testDeleteSubTaskFromId() {
+    }
+
+    public void testUpdateAnExistingRow() {
+    }
+
+    public void testUpdateAnExistingRowSubTask() {
+    }
+
+    public void testGetAllTasksList() {
+    }
+
+    public void testGetAllSubTasksList() {
+    }
+
+    public void testSelectDataFromAnId() {
+    }
+
+    public void testSelectSubDataFromAnId() {
+    }
+
+    public void testGetAllPendingTasks() {
+    }
+
+    public void testGetRequiredConverters() {
+    }
 }
