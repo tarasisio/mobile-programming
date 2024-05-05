b473eda0b2260a00bf084caabfcba29696b54b3b
Hillal-cell <sserunjogi.hillal@students.mak.ac.ug>
2024-05-05 23:19:14 +0300

    Updated the dependecies.

--- Manager/app/build.gradle.kts
+++ Manager/app/build.gradle.kts
@@ -1,4 +1,3 @@
-
 plugins {
     id("com.android.application")
 }
@@ -36,9 +35,6 @@ android {
 
 dependencies {
 
-
-
-
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
@@ -52,7 +48,6 @@ dependencies {
     implementation ("io.github.inflationx:viewpump:2.1.1")
 
     //Design
-
     implementation ("androidx.exifinterface:exifinterface:1.3.7")
     implementation ("androidx.media:media:1.7.0")
     implementation ("androidx.vectordrawable:vectordrawable-animated:1.1.0")
@@ -62,20 +57,17 @@ dependencies {
     implementation ("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation ("androidx.legacy:legacy-support-v4:1.0.0")
 
-    //ButterKnife
-//    implementation ("com.jakewharton:butterknife:10.2.3")
-//    annotationProcessor ("com.jakewharton:butterknife-compiler:10.2.3")
-
-
     //Room Database
     implementation ("androidx.room:room-runtime:2.6.1")
     annotationProcessor ("androidx.room:room-compiler:2.6.1")
 
-//    implementation ("com.github.zubairehman:AlarmManager:v1.2.0-alpha01")
+
 
     //Glide
     implementation ("com.github.bumptech.glide:glide:4.16.0")
     annotationProcessor ("com.github.bumptech.glide:compiler:4.16.0")
+
+
 
     implementation ("com.applandeo:material-calendar-view:1.9.1")
 }
